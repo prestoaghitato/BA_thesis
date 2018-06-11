98cdacf334c47202a199b35d9f1ba6ede60cf809
Marcel Ruland <ruland.marcel@gmail.com>
2018-06-11 21:16:14 +0200

    write clean.jl

--- jl/clean.jl
+++ jl/clean.jl
@@ -1,7 +1,26 @@
-# using CSV, DataFrames
 
-function main(filename)
+function clean(filename)
+    # open raw file
     open(filename) do file
-        # do stuff with file
+        lines = readlines(file)
+        # open output file
+        open("output.csv", "w") do output
+            # add header
+            header = "antecedent,succedent,confidence,occurrences,duration_sec\n"
+            write(output, header)
+            # create regex
+            r1 = r"\s\s+"
+            # iterate through lines
+            for line in lines
+                # apply modifications one after another
+                out = replace(line, ", ", ";")
+                out = replace(out, r1, ",")
+                out *= "\n"
+                out = replace(out, " ", ",")
+                # write to output
+                write(output, out)
+                # and you're so done, just end end end end it
+            end
+        end
     end
 end
