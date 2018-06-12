384cacc547ff5f29203416f04c668c61c6e2d80d
Marcel Ruland <ruland.marcel@gmail.com>
2018-06-12 12:06:10 +0200

    create rulesdict.jl

--- jl/significance.jl
+++ jl/significance.jl
@@ -1,27 +1,25 @@
 using DataFrames
 
+#==
+create a DataFrame for 
+==#
 
-function main()
-    # create output file
-    open("output/output.csv", "w") do output
-        # iterate through files in input directory
-        for filename in readdir("input")
-            # get file extension
-            extension = filename[end-3:end]
-            # do some stuff if it's a csv file
-            if extension == ".csv"
-                # open csv file
-                open("input/"*filename) do file
-                    # convert csv to DataFrame
-                    df = readtable(file)
-                    workthroughDataFrame(df)
+function gothroughfiles()
+    # iterate through files in input directory
+    for filename in readdir("input")
+        # get file extension
+        extension = filename[end-3:end]
+        # do some stuff if it's a csv file
+        if extension == ".csv"
+            # open csv file
+            open("input/"*filename) do file
+                # convert csv to DataFrame
+                df = readtable(file)
+                # iterate over DataFrame rows
+                for i in 1:size(df, 1)
+                    println(df[i, 1])
                 end
             end
         end
     end
-end
-
-
-function workthroughDataFrame(df)
-    # do stuff with DataFrame
 end
