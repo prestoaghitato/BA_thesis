0b83c5078fa2615802056ee83035b5ac6b8c4e01
Marcel Ruland <ruland.marcel@gmail.com>
2018-06-13 08:39:03 +0200

    join.jl

--- jl/join.jl
+++ jl/join.jl
@@ -15,22 +15,41 @@ function loadjdl(filename)
     return out
 end
 
+
+function savejdl(file, name)
+    #==
+    save Dict to jdl file
+    in:  Dict filename
+    out: nothing
+    ==#
+    save("output/"*name*".jld", "data", file)
+end
+
+
 function main()
     # iterate through files in input directory
+        rules_sig_prepared = Dict()
     for filename in readdir("input")
         # get file extension
         extension = filename[end-3:end]
-        # do some stuff if it's a csv file
-        if extension == ".csv"
-            # open csv file
-            open("input/"*filename) do file
-                # convert csv to DataFrame
-                df = readtable(file)
-                # iterate over DataFrame rows
-                for i in 1:size(df, 1)
-                    println(df[i, 1])
+        # if it's a jdl file, get its name and load it
+        if extension == ".jdl"
+            name = filename[1:end-4]
+            source = loadjdl(filename)
+            # is it null or real data?
+            if contains(name, "null")
+                # iterate over keys
+                for key in keys(source)
+                    if haskey(rules_sig_prepared, key)
+                        rules_sig_prepared[key]
+                    else
+                        # do stuff if key doesn't exist
+                    end
                 end
+            elseif contains(name, "real")
+                # do stuff with control data
             end
+
         end
     end
 end
